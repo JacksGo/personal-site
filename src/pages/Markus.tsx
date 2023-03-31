fda2beb52ba25032783ce9c7a3408191dbbd12df
JacksGo <jackson.golletz@gmail.com>
2023-03-30 17:36:52 -0700

    Improve page transition bounding

--- src/pages/Markus.tsx
+++ src/pages/Markus.tsx
@@ -13,34 +13,36 @@ const MarkusPage = () => {
       <Helmet>
         <title>Markus - Jackson Golletz</title>
       </Helmet>
-      <section className={styles.scrollSection}>
-        <header
-          className={styles.header}
-          aria-label="Markus"
-          onClick={() => viewTransition("/")}
-        >
-          <MarkusLogo />
-        </header>
-        <article className={styles.content}>
-          <p>
-            Markus is a web application, written in React and TypeScript, which allows a user to upload a bookmark file exported from their web browser, then generate a copy which contains only a desired subset of the input. Currently, major browsers only support full exports and imports, so this app enables a use case that is not otherwise possible.
-          </p>
-          <p>
-            While working on this project, I discovered that, despite bookmark files being imported to and exported from the browser as HTML files, they are not syntactically valid HTML. Rather, the netscape-bookmark-file format is a mangled HTML-esque type which browsers can make sense of, but which contains no <code>head</code>, <code>body</code>, or <code>html</code> tags, and has dangling <code>&lt;p&gt;</code>s (with no corresponding closing tags) at the start and end of every folder in the document. Consequently, my initial approach of parsing the file using DOMParser and XMLSerializer worked for importing the file into the tool, but not for exporting, since XMLSerializer can only produce syntactically valid markup. In order to create a bookmark file that browsers would accept, I had to iterate the selected items and concatenate them to a string, which I found upsettingly inelegant.
-          </p>
-          <p>
-            Markus was built using the latest version of Microsoft&rsquo;s Fluent UI framework.
-          </p>
-        </article>
-      </section>
-      <footer className={styles.actionTray}>
-        <button onClick={(e) => {e.preventDefault(); viewTransition("/");}} className={styles.action}>
-          Back
-        </button>
-        <a href="https://github.com/JacksGo/markus" className={styles.action}>
-          View repository
-        </a>
-      </footer>
+      <div className={styles.pageInterior}>
+        <section className={styles.scrollSection}>
+          <header
+            className={styles.header}
+            aria-label="Markus"
+            onClick={() => viewTransition("/")}
+          >
+            <MarkusLogo />
+          </header>
+          <article className={styles.content}>
+            <p>
+              Markus is a web application, written in React and TypeScript, which allows a user to upload a bookmark file exported from their web browser, then generate a copy which contains only a desired subset of the input. Currently, major browsers only support full exports and imports, so this app enables a use case that is not otherwise possible.
+            </p>
+            <p>
+              While working on this project, I discovered that, despite bookmark files being imported to and exported from the browser as HTML files, they are not syntactically valid HTML. Rather, the netscape-bookmark-file format is a mangled HTML-esque type which browsers can make sense of, but which contains no <code>head</code>, <code>body</code>, or <code>html</code> tags, and has dangling <code>&lt;p&gt;</code>s (with no corresponding closing tags) at the start and end of every folder in the document. Consequently, my initial approach of parsing the file using DOMParser and XMLSerializer worked for importing the file into the tool, but not for exporting, since XMLSerializer can only produce syntactically valid markup. In order to create a bookmark file that browsers would accept, I had to iterate the selected items and concatenate them to a string, which I found upsettingly inelegant.
+            </p>
+            <p>
+              Markus was built using the latest version of Microsoft&rsquo;s Fluent UI framework.
+            </p>
+          </article>
+        </section>
+        <footer className={styles.actionTray}>
+          <button onClick={(e) => {e.preventDefault(); viewTransition("/");}} className={styles.action}>
+            Back
+          </button>
+          <a href="https://github.com/JacksGo/markus" className={styles.action}>
+            View repository
+          </a>
+        </footer>
+      </div>
     </section>
   );
 };
--- src/pages/Quest.tsx
+++ src/pages/Quest.tsx
@@ -13,38 +13,40 @@ const QuestPage = () => {
       <Helmet>
         <title>Quest.fan - Jackson Golletz</title>
       </Helmet>
-      <section className={styles.scrollSection}>
-        <header
-          className={styles.header}
-          aria-label="Quest.fan"
-          onClick={() => viewTransition("/")}
-        >
-          <QuestLogo />
-        </header>
-        <article className={styles.content}>
-          <p>
-            Quest.fan is a web application, written in React, which allows players of the tabletop role-playing game Quest to easily create, manage, and share their characters&rsquo; information with the other players in their group.
-          </p>
-          <p>
-            The project was born out of necessity in the early days of the pandemic, at a time when meeting in person and physically sharing character sheets was neither safe nor practical. I created the first iteration of the site in just over a week using React&rsquo;s Context API for state management, then later rewrote it from scratch using Redux Toolkit as a learning exercise. In the months following the initial release, I worked diligently to add new features and address issues as they were brought to my attention.
-          </p>
-          <p>
-            From the outset, I knew that to minimize the friction of switching to a digital system, I wanted the site to function as similarly as possible to the physical game. I researched the company&rsquo;s branding, created numerous mockups, and brainstormed ways to preserve the familiarity of Quest while eliminating pain points that were present in the pen-and-paper game.
-          </p>
-          <p>
-            I also invested heavily in improving accessibility in the transition from physical to digital. In addition to conducting ad hoc, informal user testing, I extensively tested the website using multiple screen readers and referenced the WAI-ARIA standard throughout the development process.
-          </p>
-        </article>
-      </section>
-      <footer className={styles.actionTray}>
-        <button onClick={(e) => {e.preventDefault(); viewTransition("/");}} className={styles.action}>
-          Back
-        </button>
-        {/* <div className={styles.actionDivider}></div> */}
-        <a href="https://quest.fan/" className={styles.action}>
-          Visit site
-        </a>
-      </footer>
+      <div className={styles.pageInterior}>
+        <section className={styles.scrollSection}>
+          <header
+            className={styles.header}
+            aria-label="Quest.fan"
+            onClick={() => viewTransition("/")}
+          >
+            <QuestLogo />
+          </header>
+          <article className={styles.content}>
+            <p>
+              Quest.fan is a web application, written in React, which allows players of the tabletop role-playing game Quest to easily create, manage, and share their characters&rsquo; information with the other players in their group.
+            </p>
+            <p>
+              The project was born out of necessity in the early days of the pandemic, at a time when meeting in person and physically sharing character sheets was neither safe nor practical. I created the first iteration of the site in just over a week using React&rsquo;s Context API for state management, then later rewrote it from scratch using Redux Toolkit as a learning exercise. In the months following the initial release, I worked diligently to add new features and address issues as they were brought to my attention.
+            </p>
+            <p>
+              From the outset, I knew that to minimize the friction of switching to a digital system, I wanted the site to function as similarly as possible to the physical game. I researched the company&rsquo;s branding, created numerous mockups, and brainstormed ways to preserve the familiarity of Quest while eliminating pain points that were present in the pen-and-paper game.
+            </p>
+            <p>
+              I also invested heavily in improving accessibility in the transition from physical to digital. In addition to conducting ad hoc, informal user testing, I extensively tested the website using multiple screen readers and referenced the WAI-ARIA standard throughout the development process.
+            </p>
+          </article>
+        </section>
+        <footer className={styles.actionTray}>
+          <button onClick={(e) => {e.preventDefault(); viewTransition("/");}} className={styles.action}>
+            Back
+          </button>
+          {/* <div className={styles.actionDivider}></div> */}
+          <a href="https://quest.fan/" className={styles.action}>
+            Visit site
+          </a>
+        </footer>
+      </div>
     </section>
   );
 };
